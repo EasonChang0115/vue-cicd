bbc03e4390785f92d77e1f9c65bffa572f173374
EasonChang <s8710606@gmail.com>
2021-03-04 17:21:15 +0800

    test

---
+++ src/components/Counter.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="number">{{ num }}</div>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.num += 1;
+    },
+    decrement() {
+      this.num -= 1;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
