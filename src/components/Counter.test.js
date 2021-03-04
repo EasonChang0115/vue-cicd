d4335b8424e94aa7431a54e72ae8171cccb187da
EasonChang <s8710606@gmail.com>
2021-03-04 18:33:58 +0800

    feature: 新增測試內容

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/vue';
+import { fireEvent, render } from '@testing-library/vue';
 import Counter from './Counter.vue';
 
 test('Origin title is [當前點擊次數0]', () => {
@@ -6,3 +6,14 @@ test('Origin title is [當前點擊次數0]', () => {
   const title = getByTestId('currentNumber');
   expect(title.innerHTML).toBe('當前點擊次數0');
 });
+
+test('After I click button the title will change to [當前點擊次數1]', async () => {
+  const { getByTestId, getByText } = render(Counter);
+
+  const incrementButton = getByText('+');
+  await fireEvent.click(incrementButton);
+
+  const title = getByTestId('currentNumber');
+  expect(title.innerHTML).toBe('當前點擊次數10');
+});
+
